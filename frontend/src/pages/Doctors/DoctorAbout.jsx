5e9d297897e5e4446fab11e4ae3fd0707f273f24
RithikRanaweera <rithikranaweera004@gmail.com>
2024-07-27 23:07:54 +0530

    Done doctor's details

---
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const DoctorAbout = () => {
+    return (
+        <div>
+            <div>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
+                    About of
+                    <span className='text-irisBlueColor font-bold text-[24px] leading-9'>Dr. Muhibur Rahman</span>
+                </h3>
+
+                <p className='text_para'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+
+            <div className='mt-12'>
+                <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>
+                    Education
+                </h3>
+                <ul className="pt-4 md:p-5">
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                        <div>
+                            <span className='text-irisBlueColor font-semibold text-[15px] leading-6'>
+                                23 june, 2008
+                            </span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor'>
+                                PHD in Surgeon
+                            </p>
+                        </div>
+                        <p className='text-[16px] leading-6 font-medium text-textColor'>
+                            New Apollo Hospital, New York.
+                        </p>
+                    </li>
+                    <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
+                        <div>
+                            <span className='text-irisBlueColor font-semibold text-[15px] leading-6'>
+                                23 july, 2008
+                            </span>
+                            <p className='text-[16px] leading-6 font-medium text-textColor'>
+                                PHD in Surgeon
+                            </p>
+                        </div>
+                        <p className='text-[16px] leading-6 font-medium text-textColor'>
+                            New Apollo Hospital, New York.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default DoctorAbout
--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import DoctorCard from "./../../components/Doctors/DoctorCard";
+import { doctors } from "./../../assets/data/doctors"
+import Testimonial from "../../components/Testimonial/Testimonial";
 
 const Doctors = () => {
   return (
-    <div>Doctors</div>
+    <>
+      <section className="bg-[#fff9ea]">
+        <div className="container text-center">
+          <h2 className="heading">Find a Doctor</h2>
+          <div className="max-w-[570px] mx-auto mt-[30px] bg-[#0066ff2c] rounded-md flex items-center justify-between">
+            <input
+              type="search"
+              placeholder="Search doctor"
+              className="w-full bg-transparent py-4 pr-2 pl-4 placeholder:text-textColor cursor-pointer focus:outline-none"
+            />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">Search</button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container">
+          <div className="grid grid-cold-1 sm:grid-cold-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className='container'>
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">
+              What our patient say
+            </h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our health System offers unmatched,expert health care
+            </p>
+          </div>
+          <Testimonial/>
+        </div>
+      </section>
+    </>
   )
 }
 
