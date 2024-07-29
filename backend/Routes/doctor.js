d69d670890aa9e7621a01b7daf8bc35d1ba3af0f
RithikRanaweera <rithikranaweera004@gmail.com>
2024-07-29 17:44:07 +0530

    Done Backend

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -6,11 +6,20 @@ import {
   getSingleDoctor,
 } from "../Controllers/doctorController.js";
 
+import reviewRouter from "./review.js"
+
+import { authenticate, restrict } from "../auth/verifyToken.js";
+
 const router = express.Router();
+
+//nested route
+router.use("/:doctorId/reviews", reviewRouter);
 
 router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctor);
-router.put("/:id", updateDoctor);
-router.delete("/:id", deleteDoctor);
+router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
+
+
 
 export default router;
