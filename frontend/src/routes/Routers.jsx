f812f20dd5875accb0478cb9789f13bbb6c12e24
RithikRanaweera <rithikranaweera004@gmail.com>
2024-07-30 23:15:52 +0530

    Create Protected Route

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,22 +6,27 @@ import Login from "../pages/Login ";
 import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
 import Home from "../pages/Home"
+import MyAccount from '../Dashbord/user-account/MyAccount';
+import Dashboard from '../Dashbord/doctor-account/Dashboard';
+import ProtectedRoute from './ProtectedRoute';
 
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/doctors" element={<Doctors/>}/>
-      <Route path="/doctors/:id" element={<DoctorDetails/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/register" element={<Signup/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/services" element={<Services/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount /></ProtectedRoute>} />
+      <Route path="/doctors/profile/me" element={<ProtectedRoute allowedRoles={["doctor"]}><Dashboard /></ProtectedRoute>} />
     </Routes>
   )
 }
 
-export default Routers
+export default Routers;
