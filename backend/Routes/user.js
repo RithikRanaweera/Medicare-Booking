7ca357196ee355a223d783b1cd4c70674695ac19
RithikRanaweera <rithikranaweera004@gmail.com>
2024-07-30 19:47:56 +0530

    Backend for appointment and get Doctor profile

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -4,20 +4,24 @@ import {
   deleteUser,
   getAllUser,
   getSingleUser,
+  getUserProfile,
+  getMyAppointments
 } from "../Controllers/userController.js";
 
 import {authenticate, restrict} from "../auth/verifyToken.js"
-
-import reviewRouter from "./review.js";
-
 const router = express.Router();
 
+//13,14 statement are not in this video
+import reviewRouter from "./review.js";
 //nested route
 router.use("/:doctorId/reviews", reviewRouter);
+
 
 router.get("/:id", authenticate, restrict(["patient"]), getSingleUser);
 router.get("/", authenticate, restrict(["admin"]), getAllUser);
 router.put("/:id", authenticate, restrict(["patient"]), updateUser);
 router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
+router.get("/profile/me", authenticate, restrict(["patient"]), getUserProfile);
+router.get("appointments/my-appointments", authenticate, restrict(["patient"]), getMyAppointments);
 
 export default router;
