1c7f2e2aa48440d4ee08bd0dee6896fad875dd34
RithikRanaweera <rithikranaweera004@gmail.com>
2024-08-04 13:29:26 +0530

    Done Payment section

--- frontend/src/pages/Doctors/SlidePanel.jsx
+++ frontend/src/pages/Doctors/SlidePanel.jsx
@@ -1,6 +1,37 @@
 import convertTime from "../../utils/convertTime"
+import { BASE_URL, token } from "./../../config";
+import { toast } from "react-toastify";
 
 const SlidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
+
+    const bookingHandler = async () => {
+
+        try {
+            const res = await fetch(`${BASE_URL}/bookings/checkout-session/${doctorId}`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            const data = await res.json()
+
+            if (!res.ok) {
+                throw new Error(data.message + 'please try again')
+            }
+
+            if (data.session.url) {
+                window.location.href = data.session.url
+            }
+
+        } catch (err) {
+            toast.error(err.message)
+        }
+
+
+
+    }
+
     return (
         <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
             <div className="flex items-center justify-between">
@@ -19,7 +50,7 @@ const SlidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
                     {timeSlots?.map((item, index) => (
                         <li key={index} className="flex items-center justify-between mb-2">
                             <p className="text-[15px] leading-6 font-semibold text-textColor">
-                               {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
+                                {item.day.charAt(0).toUpperCase() + item.day.slice(1)}
                             </p>
                             <p className="text-[15px] leading-6 font-semibold text-textColor">
                                 {convertTime(item.startingTime)} - {convertTime(item.endingTime)}
@@ -29,7 +60,9 @@ const SlidePanel = ({ doctorId, ticketPrice, timeSlots }) => {
 
                 </ul>
             </div>
-            <button className="btn px-2 w-full rounded-md">Booking Appointment</button>
+            <button onClick={bookingHandler} className="btn px-2 w-full rounded-md">
+                Booking Appointment
+            </button>
         </div>
     )
 }
